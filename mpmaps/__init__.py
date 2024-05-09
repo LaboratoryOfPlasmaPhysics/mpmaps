f5745b3648f5aa546ceeb706e73e09a033e5108a
Nicolas Aunai <nicolas.aunai@lpp.polytechnique.fr>
2024-05-09 16:51:03 +0200

    Bump version: 0.1.2 → 0.2.0

--- mpmaps/__init__.py
+++ mpmaps/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Nicolas Aunai"""
 __email__ = "nicolas.aunai@lpp.polytechnique.fr"
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 
 from platformdirs import user_data_dir
 import urllib.request
